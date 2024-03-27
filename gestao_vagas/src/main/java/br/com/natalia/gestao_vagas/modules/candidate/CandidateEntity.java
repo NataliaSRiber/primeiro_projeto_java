96c14eb5cc9ef8cfee4f311142bfc662560fe72b
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2024-03-27 15:53:14 -0300

    Adds @Entity annotation to mark class as JPA entity for database table creation

--- gestao_vagas/src/main/java/br/com/natalia/gestao_vagas/modules/candidate/CandidateEntity.java
+++ gestao_vagas/src/main/java/br/com/natalia/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,17 +1,27 @@
 package br.com.natalia.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 // dependencia instalada para utilizar os get e set automaticamente devido ao private dos dados do candidate
 import lombok.Data;
 
 @Data
+// para a criação da tabela candidate( h]jpa subentendem-se que cada uma destas informações será uma coluna no db)
+@Entity(name = "candidate")
 public class CandidateEntity {
   
+  @Id
+  @GeneratedValue(strategy = GenerationType. UUID)
   private UUID id;
   private String name;
 
@@ -27,4 +37,7 @@ public class CandidateEntity {
   private String description;
   private String curriculum;
 
+  // coluna de createdAt
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
