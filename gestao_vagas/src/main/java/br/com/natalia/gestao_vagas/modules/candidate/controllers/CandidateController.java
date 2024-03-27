58aeb802ca0f9d73ba3ab5bed24cffacbaad072b
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2024-03-27 16:04:00 -0300

    feat: Creates repository class for database connection.

--- gestao_vagas/src/main/java/br/com/natalia/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ gestao_vagas/src/main/java/br/com/natalia/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -4,8 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.natalia.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.natalia.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,11 +16,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/candidate")
 public class CandidateController {
   
+  @Autowired
+  private CandidateRepository candidateRepository;
+
   @PostMapping("/")
   // as informações do candidato virão no body da requisição @requestBody
   // @valid informa ao controller que deve ser feita a validação dos dados
-  public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
-    System.out.println("Candidato");
-    System.out.println(candidateEntity.getEmail());
+  public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    return this.candidateRepository.save(candidateEntity);
   }
 }
